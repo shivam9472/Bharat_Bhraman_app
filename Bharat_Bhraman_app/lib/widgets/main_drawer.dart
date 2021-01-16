6fdcee49e6c6eed743fb770c3604b5ffca322054
shivam9472 <59747638+shivam9472@users.noreply.github.com>
2021-01-16 18:10:39 +0530

    Update main_drawer.dart

--- Bharat_Bhraman_app/lib/widgets/main_drawer.dart
+++ Bharat_Bhraman_app/lib/widgets/main_drawer.dart
@@ -13,7 +13,6 @@ import 'package:bharat_bhraman_app/models/profile.dart';
 import 'package:bharat_bhraman_app/services/auth.dart';
 import 'package:bharat_bhraman_app/widgets/platform_alert_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 
 import 'package:provider/provider.dart';
 import '../widgets/destination_carousel.dart';
@@ -24,99 +23,159 @@ import '../widgets/events_carousel.dart';
 import '../widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
-
-class MainDrawer extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final String uid;
-  MainDrawer({Key key, this.uid}) : super(key: key);
+
+  HomeScreen({
+    Key key,
+    this.uid,
+  }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final auth = Provider.of<AuthBase>(context);
+      await auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await PlatformAlertDialog(
+      title: 'Logout',
+      content: 'Are you sure that you want to logout?',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Logout',
+    ).show(context);
+    if (didRequestSignOut == true) {
+      _signOut(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 200,
-            width: double.infinity,
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            color: Theme.of(context).accentColor,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 20,),
-                Text(
-                  'Welcome to Bharat Bhraman',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 30,
-                      color: Theme.of(context).primaryColor),
-                ),
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 18,
-                      child: ClipOval(
-                        child: Image.network(
-                          'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
-                        ),
+    final size = MediaQuery.of(context).size;
+    List<Home_item> home_list = [
+      Home_item(
+          imageurl: "https://knowindia.gov.in/assets/images/img-identity.png",
+          titile: "National Identity Element",
+          ontap: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => NationalidentityScreen()));
+          }),
+      Home_item(
+          imageurl: "https://knowindia.gov.in/assets/images/img-culture.png",
+          titile: "Cultural And Heritage",
+          ontap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (ctx) => Culturalandheritagescreen()));
+          }),
+      Home_item(
+          imageurl: "https://knowindia.gov.in/assets/images/img-states.png",
+          titile: "States/UTs",
+          ontap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (ctx) => StateScreen()));
+          }),
+      Home_item(
+          imageurl: "https://knowindia.gov.in/assets/images/img-gen-info.png",
+          titile: "General Information",
+          ontap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (ctx) => Generalinformationscreen()));
+          }),
+    ];
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.orangeAccent, Colors.yellowAccent]),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Bharat Bhraman"),
+          actions: [
+//            FlatButton(
+//              child: Text('horders'),
+//              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+//                  builder: (ctx) => DestinationOrderScreen(
+//                        uid: widget.uid,
+//                      ))),
+//            ),
+
+            IconButton(
+              onPressed: () => _confirmSignOut(context),
+              icon: Icon(Icons.logout),
+            ),
+          ],
+        ),
+        drawer: MainDrawer(uid: widget.uid),
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            children: <Widget>[
+              GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: home_list.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 1),
+                itemBuilder: (context, i) {
+                  return GestureDetector(
+                    onTap: home_list[i].ontap,
+                    child: Container(
+                      height: size.height * 0.25,
+                      width: size.width * 0.5,
+                      child: Column(
+                        children: [
+                          Image.network(
+                            home_list[i].imageurl,
+                            height: size.height * 0.2,
+                            width: size.width * 0.3,
+                          ),
+                          Text(home_list[i].titile),
+                        ],
                       ),
                     ),
-                    SizedBox(width: 20,),
-                    Text('Hello athithi',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
-                  ],
+                  );
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              DestinationCarousel(
+                uid: widget.uid,
+              ), // monuments
+              SizedBox(height: 20.0),
+              HandicraftsCarousel(
+                uid: widget.uid,
+              ),
+              SizedBox(height: 20.0),
+              FestivalsCarousel(),
+              SizedBox(height: 20.0),
+              PerformingArtsCarousel(),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Text(
+                  'National Events',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 20.0),
+              CarouselDemo(),
+            ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          FlatButton(
-            padding: EdgeInsets.all(20),
-            minWidth: double.infinity,
-            height: 50,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            color: Colors.orangeAccent,
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (ctx) => ProfileLandingpage(
-                    uid: uid,
-                  )));
-            },
-            child: Text("Profile",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-           FlatButton(
-             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-              minWidth: double.infinity,
-              height: 50,
-              color: Colors.pinkAccent.shade100,
-              child: Text('Booked Tickets',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
-              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (ctx) => DestinationOrderScreen(
-                    uid: uid,
-                  ))),
-            ),
-          SizedBox(
-            height: 10,
-          ),
-
-
-          FlatButton(
-            minWidth: double.infinity,
-            height: 50,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-            color: Colors.indigo,
-            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                builder: (ctx) => OrderScreen(
-                  uid: uid,
-                ))),
-            child: Text("Your Cart",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-          )
-        ],
+        ),
       ),
     );
   }
