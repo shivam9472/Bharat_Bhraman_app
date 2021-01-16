1e574725526918701812dd7cc4ff7951cb0733d6
distroyerkant <55324333+distroyerkant@users.noreply.github.com>
2021-01-16 16:14:08 +0530

    Create main_drawer.dart

---
+++ Bharat_Bhraman_app/lib/widgets/main_drawer.dart
@@ -0,0 +1,123 @@
+import 'package:bharat_bhraman_app/Screens/cultureandheritagescreen.dart';
+import 'package:bharat_bhraman_app/Screens/destinationorderscreen.dart';
+import 'package:bharat_bhraman_app/Screens/generlinformationscreen.dart';
+
+import 'package:bharat_bhraman_app/Screens/nationalidentityscreen.dart';
+import 'package:bharat_bhraman_app/Screens/orderscreen.dart';
+import 'package:bharat_bhraman_app/Screens/profile_landingpage.dart';
+import 'package:bharat_bhraman_app/Screens/profilescreen.dart';
+import 'package:bharat_bhraman_app/Screens/statesscreen.dart';
+import 'package:bharat_bhraman_app/models/home_list.dart';
+import 'package:bharat_bhraman_app/models/profile.dart';
+
+import 'package:bharat_bhraman_app/services/auth.dart';
+import 'package:bharat_bhraman_app/widgets/platform_alert_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:provider/provider.dart';
+import '../widgets/destination_carousel.dart';
+import '../widgets/handicrafts_carousel.dart';
+import '../widgets/festivals_carousel.dart';
+import '../widgets/performing_arts_carousel.dart';
+import '../widgets/events_carousel.dart';
+import '../widgets/main_drawer.dart';
+import 'package:flutter/material.dart';
+
+
+class MainDrawer extends StatelessWidget {
+  final String uid;
+  MainDrawer({Key key, this.uid}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 200,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20,),
+                Text(
+                  'Welcome to Bharat Bhraman',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 30,
+                      color: Theme.of(context).primaryColor),
+                ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 18,
+                      child: ClipOval(
+                        child: Image.network(
+                          'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Text('Hello athithi',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          FlatButton(
+            padding: EdgeInsets.all(20),
+            minWidth: double.infinity,
+            height: 50,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            color: Colors.orangeAccent,
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (ctx) => ProfileLandingpage(
+                    uid: uid,
+                  )));
+            },
+            child: Text("Profile",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+           FlatButton(
+             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              minWidth: double.infinity,
+              height: 50,
+              color: Colors.pinkAccent.shade100,
+              child: Text('Booked Tickets',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (ctx) => DestinationOrderScreen(
+                    uid: uid,
+                  ))),
+            ),
+          SizedBox(
+            height: 10,
+          ),
+
+
+          FlatButton(
+            minWidth: double.infinity,
+            height: 50,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            color: Colors.indigo,
+            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (ctx) => OrderScreen(
+                  uid: uid,
+                ))),
+            child: Text("Your Cart",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+          )
+        ],
+      ),
+    );
+  }
+}
